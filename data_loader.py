a83aec0e94aea4cec4a0d64682303cce9c37a79a
liuyoshio <jiaweiliu007@163.com>
2022-05-15 23:42:30 +0900

    qi ta xian bie guan,hai mei gao hao

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,51 @@
-from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+import seaborn as sns
 import cv2
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import model_utils
 
+utils = model_utils.utils()
+
+train_dir = '/Volumes/T7/herbarium-2022-fgvc9/train_images/'
+test_dir = '/Volumes/T7/herbarium-2022-fgvc9/test_images/'
+
+with open('/Volumes/T7/herbarium-2022-fgvc9/train_metadata.json') as json_file:
+    train_meta = json.load(json_file)
+with open("/Volumes/T7/herbarium-2022-fgvc9/test_metadata.json") as json_file:
+    test_meta = json.load(json_file)
+
+image_ids = [image["image_id"] for image in train_meta["images"]]
+image_dirs = [train_dir + image['file_name'] for image in train_meta["images"]]
+category_ids = [annotation['category_id'] for annotation in train_meta['annotations']]
+genus_ids = [annotation['genus_id'] for annotation in train_meta['annotations']]
+
+test_ids = [image['image_id'] for image in test_meta]
+test_dirs = [test_dir + image['file_name'] for image in test_meta]
+
+train_df = pd.DataFrame({
+    "image_id" : image_ids,
+    "image_dir" : image_dirs,
+    "category" : category_ids,
+    "genus" : genus_ids})
+
+test_df = pd.DataFrame({
+    "test_id" : test_ids,
+    "test_dir" : test_dirs
+})
+
+genus_map = {genus['genus_id'] : genus['genus'] for genus in train_meta['genera']}
+train_df['genus'] = train_df['genus'].map(genus_map)
+
+X_Train, Y_Train = train_df['image_dir'].values, train_df['category'].values
+    
 class GetData(Dataset):
     def __init__(self, FNames, Labels, Transform):
         self.fnames = FNames
@@ -19,7 +64,7 @@ class GetData(Dataset):
             return self.transform(x), self.fnames[index]
                 
 trainset = GetData(X_Train, Y_Train, Transform)
-trainloader = DataLoader(trainset, batch_size=BATCH, shuffle=True)
+trainloader = DataLoader(trainset, batch_size= utils.BATCH, shuffle=True)
 
 N_Classes = train_df['category'].nunique()
 next(iter(trainloader))[0].shape
